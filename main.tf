dfe2ce35e1dd19e9b68a1662ef64543f8b83fac8
Arunkumar G <104412259+arunk-tx@users.noreply.github.com>
2023-05-09 19:04:59 +0530

    Updated script

--- main.tf
+++ main.tf
@@ -1,19 +1,38 @@
 provider "aws" {
     region = "ap-south-1"
 }
+#Creation of vpc
+resource "aws_vpc" "main" {
+  cidr_block       = "10.4"
+  instance_tenancy = "dedicated"
+  enable_dns_support = "true"
+  enable_dns_hostnames = "false"
 
-//s3
+   tags = {
+    Name = "no"
+  }
+}
 
-resource "aws_s3_bucket" "dss" {
-    bucket = "dss"
-    acl = "private"
-    
-    versioning {
-        enabled = false
-    }
- 
+
+#public subnet
+resource "aws_subnet" "tag1" {
+  vpc_id = aws_vpc.main.id,
+  cidr_block = "10.3.6",
+  map_public_ip_on_launch = "true",
+  availability_zone = "ap-south-1a"
+
     tags = {
-        name = "fd"
-    }
+    Name = "tag1"
+  }
+}
+#public subnet
+resource "aws_subnet" "tag2" {
+  vpc_id = aws_vpc.main.id,
+  cidr_block = "10.7.8",
+  map_public_ip_on_launch = "false",
+  availability_zone = "ap-south-1b"
 
+    tags = {
+    Name = "tag2"
+  }
 }
