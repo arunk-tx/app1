cbdb3fb4a326dcbf11d0243b8e9285c7e6042172
Arunkumar G <104412259+arunk-tx@users.noreply.github.com>
2023-05-16 13:10:55 +0530

    Updated script

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
 provider "aws" {
-    region = "sa"
+    region = "ap-south-1"
 }
 
-sas
+//s3
+
+resource "aws_s3_bucket" "df" {
+    bucket = "df"
+    acl = "private"
+    
+    versioning {
+        enabled = false
+    }
+ 
+    tags = {
+        name = "ds"
+    }
+
+}
