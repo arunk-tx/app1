13c37d21c5b716f3cdfbec45212ec0c4e6916248
Arunkumar G <104412259+arunk-tx@users.noreply.github.com>
2023-06-16 11:29:28 +0530

    Updated script

--- main.tf
+++ main.tf
@@ -1,19 +1,42 @@
 provider "aws" {
-    region = "ap-south-1"
+    region = "asa"
 }
 
-//s3
+# Create the EKS cluster
+resource "aws_eks_cluster" "my_cluster" {
+  name     = "my-cluster"
+  role_arn = aws_iam_role.my_cluster_role.arn
+  version  = "1.21"  # Replace with your desired EKS version
 
-resource "aws_s3_bucket" "singhshubham" {
-    bucket = "singhshubham"
-    acl = "public"
-    
-    versioning {
-        enabled = false
-    }
- 
-    tags = {
-        name = "a"
-    }
+  vpc_config {
+    subnet_ids = ["subnet-12345678", "subnet-23456789"]  # Replace with your desired subnet IDs
+  }
 
+  depends_on = [aws_iam_role_policy_attachment.my_cluster_attachment]
+}
+
+# Define the IAM role for the EKS cluster
+resource "aws_iam_role" "my_cluster_role" {
+  name = "my-cluster-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+# Attach the necessary policies to the IAM role
+resource "aws_iam_role_policy_attachment" "my_cluster_attachment" {
+  role       = aws_iam_role.my_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
